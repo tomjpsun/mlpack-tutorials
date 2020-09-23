6efc9ec4259023f4cc87628e97ad389429455c89
Ching-Ping Sun <tomjpsun@gmail.com>
2020-09-23 22:23:41 +0800

    add neighbor_search

--- knn_tutorial/knn_cli.sh
+++ knn_tutorial/knn_cli.sh
@@ -12,3 +12,7 @@ echo '----------'
 echo 'One dataset, 3 nearest neighbors, leaf size of 15 points'
 mlpack_knn -r reference_dataset.csv -n neighbors_out_3.csv -d distances_out_3.csv -k 3 -l 15 -v
 echo '----------'
+echo 'c++ 5 nearest neighbors on a single dataset'
+make k_neighbor_search
+echo 'use dataset self as query set, this is also called naive search'
+./k_neighbor_search
