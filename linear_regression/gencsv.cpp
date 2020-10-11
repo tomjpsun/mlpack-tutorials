364f7f9cab292eea9326adeef62bc54a1ab12c7b
Ching-Ping Sun <tomjpsun@gmail.com>
2020-10-12 00:15:36 +0800

    [Linear Regression] refactor gencsv

--- linear_regression/gencsv.cpp
+++ linear_regression/gencsv.cpp
@@ -22,6 +22,8 @@ int main(int argc, char** argv)
 	const int ROW=100;
 	const int COL=2;
 	bool uniform = false;
+	const string data_filename("dataset.csv");
+	const string pred_filename("predict.csv");
 
 	std::mt19937 engine;  // Mersenne twister random number engine
 	std::normal_distribution<double> distr(0, 0.1);
@@ -38,14 +40,19 @@ int main(int argc, char** argv)
 				     (uniform)? idx + distr(engine): double(idx) };
 	}
 	colvec C = randu<colvec>( ROW );
-
-	A.save( "dataset.csv", csv_ascii );
+	A.save( data_filename, csv_ascii );
+	cout << "save " << data_filename
+	     << "(" << A.n_rows << "," << A.n_cols << ")" << endl;
 
 	// generate predict.csv
 	mat P(3, 1);
 	for (int idx = 0; idx < 3; idx++) {
 		P.row(idx) = rowvec{ double(idx+2) };
 	}
-	P.save( "predict.csv", csv_ascii );
+	P.save( pred_filename, csv_ascii );
+	cout << "save " << pred_filename
+	     << "(" << P.n_rows << "," << P.n_cols << ")" << endl;
+
         //debug_dump( data_file );
+	return 0;
 }
--- linear_regression/lr_cli.sh
+++ linear_regression/lr_cli.sh
@@ -15,3 +15,12 @@ echo 'Prediction using a precomputed model'
 mlpack_linear_regression --input_model_file lr.xml \
 			 --test_file predict.csv \
 			 --output_predictions_file predictions2.csv -v
+echo '----------'
+echo 'Using ridge regression'
+mlpack_linear_regression --training_file dataset.csv -v --lambda 0.5 -M lr.xml
+echo 'Verify the model'
+mlpack_linear_regression --input_model_file lr.xml \
+			 --test_file predict.csv \
+			 --output_predictions_file predictions3.csv -v
+echo '----------'
+echo 'Generating a model'
